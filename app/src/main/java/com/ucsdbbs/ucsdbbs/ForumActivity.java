8b44f88c480a978b353a3867b476f50f2e64e61b
WhenIWasAsleepI <jiy173@eng.ucsd.edu>
2016-01-10 13:49:26 -0800

    view forum updated

--- app/src/main/java/com/ucsdbbs/ucsdbbs/ForumActivity.java
+++ app/src/main/java/com/ucsdbbs/ucsdbbs/ForumActivity.java
@@ -1,31 +1,38 @@
 package com.ucsdbbs.ucsdbbs;
 
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class ForumActivity extends AppCompatActivity {
-    private CategoryAdapter mCustomBaseAdapter;
+    private ForumAdapter mCustomBaseAdapter;
+    private ServerRunnable runnable;
+    // 数据
+    private ArrayList<ForumCategory> listData= new ArrayList<ForumCategory>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forum);
-        ListView listView = (ListView) findViewById(R.id.forumlist);
-        listView.setFooterDividersEnabled(false);
-        // 数据
-        ArrayList<Category> listData = getData();
 
-        mCustomBaseAdapter = new CategoryAdapter(getBaseContext(), listData);
+        getData();
 
-        // 适配器与ListView绑定
-        listView.setAdapter(mCustomBaseAdapter);
 
-        listView.setOnItemClickListener(new ItemClickListener());
     }
 
     private class ItemClickListener implements AdapterView.OnItemClickListener {
@@ -33,14 +40,14 @@ public class ForumActivity extends AppCompatActivity {
         @Override
         public void onItemClick(AdapterView<?> parent, View view, int position,
                                 long id) {
-            Toast.makeText(getBaseContext(), (String) mCustomBaseAdapter.getItem(position),
-                    Toast.LENGTH_SHORT).show();
+            //Toast.makeText(getBaseContext(), (String) mCustomBaseAdapter.getItem(position),
+            //        Toast.LENGTH_SHORT).show();
         }
 
     }
 
-    private ArrayList<Category> getData() {
-        ArrayList<Category> listData = new ArrayList<Category>();
+    private void getData() {
+    /*    ArrayList<Category> listData = new ArrayList<Category>();
         Category categoryOne = new Category("路人甲");
         categoryOne.addItem("浏览讨论区");
         Category categoryTwo = new Category("事件乙");
@@ -50,8 +57,51 @@ public class ForumActivity extends AppCompatActivity {
 
         listData.add(categoryOne);
         listData.add(categoryTwo);
-        listData.add(categoryThree);
+        listData.add(categoryThree);*/
 
-        return listData;
+        Map<String, String> map = new HashMap<String, String>();
+        runnable = new ServerRunnable("http://www.ucsdbbs.com/app_related/getforumstruct.php", map, fill_handler);
+        new Thread(runnable).start();
     }
+
+    public Handler fill_handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            List<ForumCategory> pool =new ArrayList<ForumCategory>();
+            Bundle data = msg.getData();
+            String val = data.getString("result");
+            if (val.equals("success") && !data.getString("data").equals("null")) {
+                try {
+                    Log.e("log_tag", "BACK VALUE " + data.getString("data"));
+                    JSONArray jArray = new JSONArray(data.getString("data"));
+                    int count=0;
+                    for(int i=0;i<jArray.length();i++){
+                        JSONObject json_data = jArray.getJSONObject(i);
+                        if(json_data.getString("type").equals("group")){
+                            pool.add( new ForumCategory(json_data.getString("name")));
+                            count++;
+                        }
+                        else{
+                            pool.get(count-1).addItem(json_data.getString("fid"), json_data.getString("type"), json_data.getString("name"), json_data.getString("status"), json_data.getString("displayorder"), json_data.getString("threads"), json_data.getString("posts"), json_data.getString("todayposts"));
+                        }
+
+                    }
+                    for(int i=0;i<count;i++){
+                        listData.add(pool.get(i));
+                    }
+                    ListView listView = (ListView) findViewById(R.id.forumlist);
+                    listView.setFooterDividersEnabled(false);
+                    mCustomBaseAdapter = new ForumAdapter(getBaseContext(), listData);
+
+                    // 适配器与ListView绑定
+                    listView.setAdapter(mCustomBaseAdapter);
+
+                    listView.setOnItemClickListener(new ItemClickListener());
+                }catch (JSONException e) {
+                    Log.e("log_tag", "Error parsing data " + e.toString());
+                }
+            }
+        }
+    };
 }
--- app/src/main/java/com/ucsdbbs/ucsdbbs/ForumCategory.java
+++ app/src/main/java/com/ucsdbbs/ucsdbbs/ForumCategory.java
@@ -6,7 +6,14 @@ import java.util.List;
 public class ForumCategory {
 
     private String mCategoryName;
-    private List<String> mCategoryItem = new ArrayList<String>();
+    private List<String> fid= new ArrayList<String>();
+    private List<String> type= new ArrayList<String>();
+    private List<String> name= new ArrayList<String>();
+    private List<String> status= new ArrayList<String>();
+    private List<String> displayorder= new ArrayList<String>();
+    private List<String> threads= new ArrayList<String>();
+    private List<String> posts= new ArrayList<String>();
+    private List<String> todayposts= new ArrayList<String>();
 
     public ForumCategory(String mCategroyName) {
         mCategoryName = mCategroyName;
@@ -16,8 +23,15 @@ public class ForumCategory {
         return mCategoryName;
     }
 
-    public void addItem(String pItemName) {
-        mCategoryItem.add(pItemName);
+    public void addItem(String _fid,String _type,String _name,String _status,String _displayorder,String _threads,String _posts,String _todayposts) {
+        fid.add(_fid);
+        type.add(_type);
+        name.add(_name);
+        status.add(_status);
+        displayorder.add(_displayorder);
+        threads.add(_threads);
+        posts.add(_posts);
+        todayposts.add(_todayposts);
     }
 
     /**
@@ -30,7 +44,7 @@ public class ForumCategory {
         if (pPosition == 0) {
             return mCategoryName;
         } else {
-            return mCategoryItem.get(pPosition - 1);
+            return name.get(pPosition - 1);
         }
     }
 
@@ -39,7 +53,7 @@ public class ForumCategory {
      * @return
      */
     public int getItemCount() {
-        return mCategoryItem.size() + 1;
+        return fid.size() + 1;
     }
 
 }
